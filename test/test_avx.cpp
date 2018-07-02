ce6252ea6a4b9fd54dbeecdbbc81658eb77b8591
Toshihiro KONDA <kontoshi0317@gmail.com>
2018-07-03 03:40:11 +0900

    change test pattern

--- test/test_avx.cpp
+++ test/test_avx.cpp
@@ -1,6 +1,6 @@
 #include "avxsvvm.h"
 
 void test(xmm *x, xmm *y, xmm *z) {
-  mask m = (*x).f > (*y).f;
-  *z = m._div((*x).f , (*y).f);
+  mask m = (*x).d > (*y).d;
+  *z = m._div((*x).d , (*y).d);
 }
